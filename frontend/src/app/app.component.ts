df2a8f5bf86bd5a6fa6c39f27242c23a4b3288a6
vikranth <vikranthka@gmail.com>
2024-09-12 16:02:46 +0530

    worked on the header/navbar

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/layout/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,RouterLink,RouterLinkActive,
+            HeaderComponent,
+  ],
+
+    
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'frontend';
+}
